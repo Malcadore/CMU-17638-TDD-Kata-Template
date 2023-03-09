0d7ced730efd722cc8a6c4e6b7dcd11bf956f167
Malcadore <48132207+Malcadore@users.noreply.github.com>
2023-03-08 19:52:58 -0500

    Update tests.py
    Added better examples of using the bowling module.

--- tests.py
+++ tests.py
@@ -1,1 +1,25 @@
-# TODO: Add the test code for bowling.py in here.
+import unittest
+from bowling import Bowling
+
+
+class Testing(unittest.TestCase):
+    def test_string(self):
+        a = 'some'
+        b = 'some'
+        self.assertEqual(a, b)
+
+    def test_boolean(self):
+        a = True
+        b = True
+        self.assertEqual(a, b)
+
+    def test_bowler(self):
+        GAME = Bowling()
+        BOWLER1_NAME = 'Travis'
+        BOWLER1_ID = GAME.set_bowler(BOWLER1_NAME)
+        actual = GAME.get_bowler(BOWLER1_ID)
+        self.assertEqual(BOWLER1_NAME, actual)
+       
+
+if __name__ == '__main__':
+    unittest.main()
