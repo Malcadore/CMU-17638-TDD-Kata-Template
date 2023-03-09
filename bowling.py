245f66a9938baabf825d4cf085090d4e2a76512a
Malcadore <48132207+Malcadore@users.noreply.github.com>
2023-03-08 19:53:56 -0500

    Update bowling.py
    Added example code for the exercise.

--- bowling.py
+++ bowling.py
@@ -1,1 +1,20 @@
-# TODO:Put the code for the bowling game in here.
+
+class Bowling:
+    name = []
+
+    def set_bowler(self, bowler_name):
+        self.name.append(bowler_name)
+        return len(self.name) - 1
+
+    def get_bowler(self, bowler_id):
+        if bowler_id >= len(self.name):
+            return 'There is no such bowler'
+        return self.name[bowler_id]
+
+
+if __name__ == '__main__':
+    GAME = Bowling()
+    BOWLER1_NAME = 'Travis'
+    BOWLER1_ID = GAME.set_bowler(BOWLER1_NAME)
+    print('User {} has been added with id {}'.format(BOWLER1_NAME, BOWLER1_ID))
+    print('User associated with id 0 is ', GAME.get_bowler(BOWLER1_ID))
